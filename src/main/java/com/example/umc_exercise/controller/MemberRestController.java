1ca09ec5d0caae617e311d8c8c283632dd3dea12
SunwuPark <sunwu5678@gmail.com>
2023-11-28 17:36:31 +0900

    ADD: MemberRestController.java

---
+++ src/main/java/com/example/umc_exercise/controller/MemberRestController.java
@@ -0,0 +1,27 @@
+package com.example.umc_exercise.controller;
+
+import com.example.umc_exercise.apiPayload.ApiResponse;
+import com.example.umc_exercise.converter.MemberConverter;
+import com.example.umc_exercise.domain.Member;
+import com.example.umc_exercise.dto.*;
+import com.example.umc_exercise.service.MemberService.MemberCommandService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberRestController {
+
+  private final MemberCommandService memberCommandService;
+
+  @PostMapping("/")
+  public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
+    Member member = memberCommandService.joinMember(request);
+    return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
+  }
+}
