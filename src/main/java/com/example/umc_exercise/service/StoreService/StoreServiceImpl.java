3162d8990077a53ac97b37b70204ef17d69135a5
SunwuPark <sunwu5678@gmail.com>
2023-11-29 00:52:47 +0900

    ADD: STORE ServiceImpl

---
+++ src/main/java/com/example/umc_exercise/service/StoreService/StoreServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.umc_exercise.service.StoreService;
+
+import com.example.umc_exercise.apiPayload.code.status.ErrorStatus;
+import com.example.umc_exercise.domain.Location;
+import com.example.umc_exercise.domain.Store;
+import com.example.umc_exercise.dto.StoreRequestDTO;
+import com.example.umc_exercise.exception.handler.StoreHandler;
+import com.example.umc_exercise.repository.LocationRepository;
+import com.example.umc_exercise.repository.StoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class StoreServiceImpl implements StoreService{
+  private final StoreRepository storeRepository;
+  private final LocationRepository locationRepository;
+
+  @Override
+  public Store register(StoreRequestDTO.JoinDTO request){
+    Location location = locationRepository.findById(request.getLocation_id()).orElseThrow(() -> new StoreHandler(ErrorStatus.STORE_LOCATION_NOT_FOUND));
+    Store store = Store.builder()
+            .address(request.getAddress())
+            .score(request.getRating())
+            .name(request.getName())
+            .location(location)
+            .build();
+    return storeRepository.save(store);
+  }
+}
