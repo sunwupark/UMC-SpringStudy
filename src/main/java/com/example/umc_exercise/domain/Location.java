042a9cf1f25ef5276133b130d017660519bbd0e0
SunwuPark <sunwu5678@gmail.com>
2023-11-29 02:04:30 +0900

    UPDATE: Locaiton.java

--- src/main/java/com/example/umc_exercise/domain/Location.java
+++ src/main/java/com/example/umc_exercise/domain/Location.java
@@ -1,11 +1,14 @@
 package com.example.umc_exercise.domain;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 @Entity
 @Data
 @Table(name = "location")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Location {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
