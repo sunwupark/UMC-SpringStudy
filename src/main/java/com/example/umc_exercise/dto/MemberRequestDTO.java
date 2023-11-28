3c15d10a5c204959095442560e025cb715e2bb4a
SunwuPark <sunwu5678@gmail.com>
2023-11-28 17:35:56 +0900

    UPDATE: MemberRequestDTO.java

---
+++ src/main/java/com/example/umc_exercise/dto/MemberRequestDTO.java
@@ -0,0 +1,32 @@
+package com.example.umc_exercise.dto;
+
+import com.example.umc_exercise.validation.annotation.ExistCategories;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+import java.util.List;
+
+public class MemberRequestDTO {
+
+  @Getter
+  public static class JoinDto{
+    @NotBlank
+    String name;
+    @NotNull
+    Integer gender;
+    @NotNull
+    Integer birthYear;
+    @NotNull
+    Integer birthMonth;
+    @NotNull
+    Integer birthDay;
+    @Size(min = 5, max = 12)
+    String address;
+    @Size(min = 5, max = 12)
+    String specAddress;
+    @ExistCategories
+    List<Long> preferCategory;
+  }
+}
