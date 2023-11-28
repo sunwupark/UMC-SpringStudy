7c7bc56aad2664426c3ea7f95aef604bff7e2492
SunwuPark <sunwu5678@gmail.com>
2023-11-28 17:38:22 +0900

    ADD: MemberService.java

---
+++ src/main/java/com/example/umc_exercise/service/MemberService/MemberCommandServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.umc_exercise.service.MemberService;
+
+import com.example.umc_exercise.apiPayload.code.status.ErrorStatus;
+import com.example.umc_exercise.converter.MemberConverter;
+import com.example.umc_exercise.converter.MemberPreferConverter;
+import com.example.umc_exercise.domain.FoodCategory;
+import com.example.umc_exercise.domain.Member;
+import com.example.umc_exercise.domain.mapping.MemberPrefer;
+import com.example.umc_exercise.dto.MemberRequestDTO;
+import com.example.umc_exercise.exception.handler.FoodCategoryHandler;
+import com.example.umc_exercise.repository.FoodCategoryRepository;
+import com.example.umc_exercise.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberCommandServiceImpl implements MemberCommandService{
+
+    private final MemberRepository memberRepository;
+
+    private final FoodCategoryRepository foodCategoryRepository;
+
+    @Override
+    @Transactional
+    public Member joinMember(MemberRequestDTO.JoinDto request) {
+
+        Member newMember = MemberConverter.toMember(request);
+        List<FoodCategory> foodCategoryList = request.getPreferCategory().stream()
+                .map(category -> {
+                    return foodCategoryRepository.findById(category).orElseThrow(() -> new FoodCategoryHandler(ErrorStatus.FOOD_CATEGORY_NOT_FOUND));
+                }).collect(Collectors.toList());
+
+        List<MemberPrefer> memberPreferList = MemberPreferConverter.toMemberPreferList(foodCategoryList);
+
+        memberPreferList.forEach(memberPrefer -> {memberPrefer.setMember(newMember);});
+
+        return memberRepository.save(newMember);
+    }
+}
---
+++ src/main/java/com/example/umc_exercise/service/MemberService/MemberQueryService.java
@@ -0,0 +1,4 @@
+package com.example.umc_exercise.service.MemberService;
+
+public interface MemberQueryService {
+}
