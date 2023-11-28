d9cb62ac283b7282046ef4c0a7f265ca26c7eedd
SunwuPark <sunwu5678@gmail.com>
2023-11-29 02:07:00 +0900

    UPDATE: StoreRestController

--- src/main/java/com/example/umc_exercise/controller/StoreRestController.java
+++ src/main/java/com/example/umc_exercise/controller/StoreRestController.java
@@ -1,8 +1,12 @@
 package com.example.umc_exercise.controller;
 
+import com.example.umc_exercise.apiPayload.ApiResponse;
+import com.example.umc_exercise.converter.StoreConverter;
 import com.example.umc_exercise.domain.Store;
 import com.example.umc_exercise.dto.StoreRequestDTO;
+import com.example.umc_exercise.dto.StoreResponseDTO;
 import com.example.umc_exercise.service.StoreService.StoreService;
+import io.swagger.annotations.Api;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +20,9 @@ public class StoreRestController {
   private final StoreService storeService;
 
   @PostMapping
-  public Long registerStore(@RequestBody @Valid StoreRequestDTO.JoinDTO request) {
+  public ApiResponse<StoreResponseDTO.JoinResultDTO> registerStore(@RequestBody StoreRequestDTO.JoinDTO request) {
     Store store = storeService.register(request);
-    return store.getId();
+    return ApiResponse.onSuccess(StoreConverter.toJoinResultDTO(store));
   }
 
 }
