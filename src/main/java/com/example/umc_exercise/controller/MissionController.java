ea7c352f0f28cb8a8a0ea74708c270778b47f55e
SunwuPark <sunwu5678@gmail.com>
2023-11-29 11:40:52 +0900

    ADD: MissionController.java

---
+++ src/main/java/com/example/umc_exercise/controller/MissionController.java
@@ -0,0 +1,30 @@
+package com.example.umc_exercise.controller;
+
+import com.example.umc_exercise.apiPayload.ApiResponse;
+import com.example.umc_exercise.domain.Mission;
+import com.example.umc_exercise.dto.MissionRequest;
+import com.example.umc_exercise.dto.MissionResponse;
+import com.example.umc_exercise.service.MissionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/missions")
+public class MissionController {
+  private final MissionService missionService;
+
+  @GetMapping
+  public List<Mission> findAll(){
+    return missionService.findAll();
+  }
+
+  @PostMapping
+  public ApiResponse<MissionResponse.MissionRegisterResponseDTO> register(@RequestBody MissionRequest.MissionRegisterRequestDTO missionRegisterRequestDTO) throws IOException {
+    return ApiResponse.onSuccess(missionService.register(missionRegisterRequestDTO));
+  }
+
+}
