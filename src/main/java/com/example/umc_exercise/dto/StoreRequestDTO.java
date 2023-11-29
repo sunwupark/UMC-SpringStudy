95dc2541e5dc8caa7aaa5401e70918c49e044c36
SunwuPark <sunwu5678@gmail.com>
2023-11-29 15:38:50 +0900

    UPDATE: StoreDTO.java

--- src/main/java/com/example/umc_exercise/dto/StoreRequestDTO.java
+++ src/main/java/com/example/umc_exercise/dto/StoreRequestDTO.java
@@ -2,6 +2,8 @@ package com.example.umc_exercise.dto;
 
 import com.example.umc_exercise.domain.Location;
 import com.example.umc_exercise.validation.annotation.ExistCategories;
+import com.example.umc_exercise.validation.annotation.ExistLocation;
+import com.example.umc_exercise.validation.annotation.ExistStore;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -17,12 +19,13 @@ public class StoreRequestDTO {
   @Getter
   @NoArgsConstructor
   @AllArgsConstructor
-  public static class JoinDTO{
+  public static class registerStore{
     @NotBlank
     String name;
     @Size(min = 5, max = 12)
     String address;
     Float rating;
+    @ExistLocation
     Long location_id;
   }
 }
