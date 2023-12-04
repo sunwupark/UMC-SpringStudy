d2427ab95524cda1fedcb730996652c0a6fc749f
SunwuPark <sunwu5678@gmail.com>
2023-12-05 01:40:49 +0900

    UPDATE: ReviewRestController

--- src/main/java/com/example/umc_exercise/controller/ReviewRestController.java
+++ src/main/java/com/example/umc_exercise/controller/ReviewRestController.java
@@ -11,10 +11,20 @@ import com.example.umc_exercise.dto.StoreRequestDTO;
 import com.example.umc_exercise.dto.StoreResponseDTO;
 import com.example.umc_exercise.service.ReviewService.ReviewService;
 import com.example.umc_exercise.service.StoreService.StoreService;
+import com.example.umc_exercise.validation.annotation.ExistStore;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +36,17 @@ public class ReviewRestController {
   public ApiResponse<ReviewResponseDTO.addReviewResultDTO> registerReview(@RequestBody @Valid ReviewRequestDTO.addReview request) {
     Review review = reviewService.addReview(request);
     return ApiResponse.onSuccess(ReviewConverter.toaddReviewResultDTO(review));
+  }
+
+  @Operation(summary = "특정 가게의 리뷰 목록 조회 API",description = "특정 가게의 리뷰들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
+  @GetMapping("/{storeId}/")
+  @Parameters({
+          @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다!"),
+          @Parameter(name = "page", description = "페이지 번호, 0번이 1 페이지 입니다."),
+  })
+  public ApiResponse<ReviewResponseDTO.ReviewPreViewListDTO> getReviews(@ExistStore @PathVariable(name = "storeId") Long storeId, @RequestParam Integer page){
+    Page<Review> review = reviewService.getReview(storeId, page);
+    return ApiResponse.onSuccess(ReviewConverter.reviewPreViewListDTO(review));
   }
 
 }
