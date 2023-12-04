430f7a452c8b9856fe33551f23bc6da673489406
SunwuPark <sunwu5678@gmail.com>
2023-12-05 01:40:41 +0900

    UPDATE: ReviewResponseDTO

--- src/main/java/com/example/umc_exercise/dto/ReviewResponseDTO.java
+++ src/main/java/com/example/umc_exercise/dto/ReviewResponseDTO.java
@@ -5,7 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ReviewResponseDTO {
   @Builder
@@ -15,6 +17,29 @@ public class ReviewResponseDTO {
   public static class addReviewResultDTO{
     Long review_id;
     LocalDateTime created_at;
+  }
 
+  @Builder
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class ReviewPreViewListDTO{
+    List<ReviewPreViewDTO> reviewList;
+    Integer listSize;
+    Integer totalPage;
+    Long totalElements;
+    Boolean isFirst;
+    Boolean isLast;
+  }
+
+  @Builder
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class ReviewPreViewDTO{
+    String ownerNickname;
+    Float score;
+    String body;
+    LocalDate createdAt;
   }
 }
