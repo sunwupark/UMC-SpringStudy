49702d356ed318790515e90d4128d1e46835b0f2
SunwuPark <sunwu5678@gmail.com>
2023-12-05 01:40:24 +0900

    UPDATE: ReviewRepository

--- src/main/java/com/example/umc_exercise/repository/ReviewRepository.java
+++ src/main/java/com/example/umc_exercise/repository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.example.umc_exercise.repository;
 
 import com.example.umc_exercise.domain.Review;
+import com.example.umc_exercise.domain.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+  Page<Review> findAllByStore(Store store, PageRequest pageRequest);
 }
