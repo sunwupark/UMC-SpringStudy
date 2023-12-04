2dc13dc639c04b74330b0fcec85ef9231cde7a05
SunwuPark <sunwu5678@gmail.com>
2023-12-05 01:40:57 +0900

    UPDATE: ReviewService

--- src/main/java/com/example/umc_exercise/service/ReviewService/ReviewService.java
+++ src/main/java/com/example/umc_exercise/service/ReviewService/ReviewService.java
@@ -2,7 +2,10 @@ package com.example.umc_exercise.service.ReviewService;
 
 import com.example.umc_exercise.domain.Review;
 import com.example.umc_exercise.dto.ReviewRequestDTO;
+import org.springframework.data.domain.Page;
 
 public interface ReviewService {
   public Review addReview(ReviewRequestDTO.addReview review);
+
+  public Page<Review> getReview(Long shop_id, Integer page);
 }
