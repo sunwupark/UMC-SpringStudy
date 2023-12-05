bc81e2927196959477255fb048807c278f9e25a0
SunwuPark <sunwu5678@gmail.com>
2023-12-05 22:11:26 +0900

    UPDATE: MemberMissionRequestDTO.java

--- src/main/java/com/example/umc_exercise/dto/MemberMissionRequestDTO.java
+++ src/main/java/com/example/umc_exercise/dto/MemberMissionRequestDTO.java
@@ -1,6 +1,8 @@
 package com.example.umc_exercise.dto;
 
 import com.example.umc_exercise.validation.annotation.ExistChallengingMissions;
+import com.example.umc_exercise.validation.annotation.ExistMember;
+import com.example.umc_exercise.validation.annotation.ExistMission;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +16,18 @@ public class MemberMissionRequestDTO {
   @ExistChallengingMissions(message = "이미 도전 중인 미션입니다.", memberId = "memberId", missionId = "missionId")
   public static class registerMemberMissionDTO{
     Long memberId;
+    Long missionId;
+  }
+
+  @Getter
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Builder
+  public static class toMissionCompleteRequestDTO{
+    @ExistMember
+    Long memberId;
+
+    @ExistMission
     Long missionId;
   }
 }
