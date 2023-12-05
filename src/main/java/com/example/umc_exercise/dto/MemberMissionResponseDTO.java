aee044c9895642e96f92b7352fa7cdcd37e2e90b
SunwuPark <sunwu5678@gmail.com>
2023-12-05 22:11:35 +0900

    UPDATE: MemberMissionResponseDTO.java

--- src/main/java/com/example/umc_exercise/dto/MemberMissionResponseDTO.java
+++ src/main/java/com/example/umc_exercise/dto/MemberMissionResponseDTO.java
@@ -6,7 +6,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MemberMissionResponseDTO {
 
@@ -17,5 +19,36 @@ public class MemberMissionResponseDTO {
   public static class registerResponseDTO{
     Long memberMissionId;
     LocalDateTime createdAt;
+  }
+
+  @Getter
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Builder
+  public static class toMissionCompleteResponseDTO{
+    Long memberId;
+    Long missionId;
+  }
+
+  @Builder
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class MissionByMemberMissionResponseDTO {
+    Long memberId;
+    Long missionId;
+  }
+
+  @Builder
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class MissionByMemberMissionResponseListDTO {
+    List<MemberMissionResponseDTO.MissionByMemberMissionResponseDTO> memberMissionList;
+    Integer listSize;
+    Integer totalPage;
+    Long totalElements;
+    Boolean isFirst;
+    Boolean isLast;
   }
 }
