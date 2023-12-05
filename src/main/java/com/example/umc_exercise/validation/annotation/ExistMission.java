e645c826fee240651358b520cf523b0ba74a0733
SunwuPark <sunwu5678@gmail.com>
2023-12-05 22:10:03 +0900

    ADD: ExistMission.java

---
+++ src/main/java/com/example/umc_exercise/validation/annotation/ExistMission.java
@@ -0,0 +1,17 @@
+package com.example.umc_exercise.validation.annotation;
+
+import com.example.umc_exercise.validation.validator.MemberExistValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = MemberExistValidator.class)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExistMission {
+    String message() default "해당 미션이 존재하지 않습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
