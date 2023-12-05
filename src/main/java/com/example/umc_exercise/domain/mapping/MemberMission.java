ae1bab09f6edfb0b3ea214346ff32cc3b05cb59e
SunwuPark <sunwu5678@gmail.com>
2023-12-05 22:10:26 +0900

    UPDATE: MemberMission.java

--- src/main/java/com/example/umc_exercise/domain/mapping/MemberMission.java
+++ src/main/java/com/example/umc_exercise/domain/mapping/MemberMission.java
@@ -29,4 +29,8 @@ public class MemberMission extends BaseEntity {
   @JoinColumn(name = "mission_id")
   private Mission mission;
 
+
+  public void setStatus(MissionStatus status) {
+    this.status = status;
+  }
 }
