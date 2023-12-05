f94b99def99464660c2bd275e9e95089d5fc7e8b
SunwuPark <sunwu5678@gmail.com>
2023-12-05 22:10:35 +0900

    UPDATE: MemberMissionController.java

--- src/main/java/com/example/umc_exercise/controller/MemberMissionController.java
+++ src/main/java/com/example/umc_exercise/controller/MemberMissionController.java
@@ -1,20 +1,19 @@
 package com.example.umc_exercise.controller;
 
 import com.example.umc_exercise.apiPayload.ApiResponse;
-import com.example.umc_exercise.domain.Mission;
-import com.example.umc_exercise.domain.mapping.MemberMission;
+import com.example.umc_exercise.converter.MemberMissionConverter;
 import com.example.umc_exercise.dto.MemberMissionRequestDTO;
 import com.example.umc_exercise.dto.MemberMissionResponseDTO;
-import com.example.umc_exercise.dto.MissionRequest;
-import com.example.umc_exercise.dto.MissionResponse;
 import com.example.umc_exercise.service.MemberMissionService.MemberMissionService;
-import com.example.umc_exercise.service.MissionService.MissionService;
+import com.example.umc_exercise.service.MemberService.MemberQueryService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,14 +21,17 @@ import java.util.List;
 public class MemberMissionController {
   private final MemberMissionService memberMissionService;
 
-  @GetMapping
-  public List<MemberMission> findAll(){
-    return memberMissionService.findAll();
-  }
-
+  @Operation(summary = "미션 등록하기 API",description = "미션을 멤버에 등록하는 API입니다")
   @PostMapping
   public ApiResponse<MemberMissionResponseDTO.registerResponseDTO> register(@RequestBody @Valid MemberMissionRequestDTO.registerMemberMissionDTO missionRegisterRequestDTO){
-    return ApiResponse.onSuccess(memberMissionService.registerMission(missionRegisterRequestDTO));
+    return ApiResponse.onSuccess(MemberMissionConverter.toRegisterResponseDTO(memberMissionService.registerMission(missionRegisterRequestDTO)));
   }
+  @Operation(summary = "진행중인 미션 진행 완료로 바꾸기 API",description = "진행중인 미션 진행 완료로 바꾸는 API입니다")
+  @PostMapping("/complete")
+  public ApiResponse<MemberMissionResponseDTO.toMissionCompleteResponseDTO> toMissionComplete(@RequestBody @Valid MemberMissionRequestDTO.toMissionCompleteRequestDTO missionCompleteRequestDTO){
+    return ApiResponse.onSuccess(MemberMissionConverter.toMissionCompleteResponseDTO(memberMissionService.missionComplete(missionCompleteRequestDTO)));
+  }
+
+
 
 }
