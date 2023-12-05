4f5ab2435f0ed07a8b6e8623b041c4518987357f
SunwuPark <sunwu5678@gmail.com>
2023-12-05 22:14:35 +0900

    UPDATE: StoreRestController

--- src/main/java/com/example/umc_exercise/service/StoreService/StoreService.java
+++ src/main/java/com/example/umc_exercise/service/StoreService/StoreService.java
@@ -1,7 +1,9 @@
 package com.example.umc_exercise.service.StoreService;
 
+import com.example.umc_exercise.domain.Mission;
 import com.example.umc_exercise.domain.Store;
 import com.example.umc_exercise.dto.StoreRequestDTO;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface StoreService {
--- src/main/java/com/example/umc_exercise/service/StoreService/StoreServiceImpl.java
+++ src/main/java/com/example/umc_exercise/service/StoreService/StoreServiceImpl.java
@@ -2,12 +2,16 @@ package com.example.umc_exercise.service.StoreService;
 
 import com.example.umc_exercise.apiPayload.code.status.ErrorStatus;
 import com.example.umc_exercise.domain.Location;
+import com.example.umc_exercise.domain.Mission;
 import com.example.umc_exercise.domain.Store;
 import com.example.umc_exercise.dto.StoreRequestDTO;
 import com.example.umc_exercise.exception.handler.StoreHandler;
 import com.example.umc_exercise.repository.LocationRepository;
+import com.example.umc_exercise.repository.MissionRepository;
 import com.example.umc_exercise.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,6 +21,7 @@ import java.util.Optional;
 public class StoreServiceImpl implements StoreService{
   private final StoreRepository storeRepository;
   private final LocationRepository locationRepository;
+  private final MissionRepository missionRepository;
 
   @Override
   public Store register(StoreRequestDTO.registerStore request){
