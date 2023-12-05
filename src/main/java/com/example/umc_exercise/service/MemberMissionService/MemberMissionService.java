c3fcb3bedeb792ce590b06dd22618f7053c880be
SunwuPark <sunwu5678@gmail.com>
2023-12-05 22:11:44 +0900

    UPDATE: MemberMissionService.java

--- src/main/java/com/example/umc_exercise/service/MemberMissionService/MemberMissionService.java
+++ src/main/java/com/example/umc_exercise/service/MemberMissionService/MemberMissionService.java
@@ -2,17 +2,17 @@ package com.example.umc_exercise.service.MemberMissionService;
 
 import com.example.umc_exercise.domain.mapping.MemberMission;
 import com.example.umc_exercise.dto.MemberMissionRequestDTO;
-import com.example.umc_exercise.dto.MemberMissionResponseDTO;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberMissionService {
-  public MemberMissionResponseDTO.registerResponseDTO registerMission(MemberMissionRequestDTO.registerMemberMissionDTO memberMissionRequestDTO);
+  public MemberMission registerMission(MemberMissionRequestDTO.registerMemberMissionDTO memberMissionRequestDTO);
   public List<MemberMission> findAll();
 
   public Optional<MemberMission> findById(Long id);
 
   public Optional<MemberMission> findByMissionIdAndMemberId(Long MissionId, Long MemberId);
 
+  public MemberMission missionComplete(MemberMissionRequestDTO.toMissionCompleteRequestDTO missionCompleteRequestDTO);
 }
